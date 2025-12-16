1f9178f87ffc23da159ba94d277d076a33bcc4a0
Alex Balint <alexbalint26@gmail.com>
2025-12-16 18:51:11 +0200

    display graphic scene + window resize

--- GraphicScene_OpenGL/Model3D.cpp
+++ GraphicScene_OpenGL/Model3D.cpp
@@ -20,135 +20,263 @@ namespace gps {
 			meshes[i].Draw(shaderProgram);
 	}
 
-	// Does the parsing of the .obj file and fills in the data structure
-	void Model3D::ReadOBJ(std::string fileName, std::string basePath) {
+	//// Does the parsing of the .obj file and fills in the data structure
+	//void Model3D::ReadOBJ(std::string fileName, std::string basePath) {
 
-        std::cout << "Loading : " << fileName << std::endl;
-		tinyobj::attrib_t attrib;
-		std::vector<tinyobj::shape_t> shapes;
-		std::vector<tinyobj::material_t> materials;
-		int materialId;
+ //       std::cout << "Loading : " << fileName << std::endl;
+	//	tinyobj::attrib_t attrib;
+	//	std::vector<tinyobj::shape_t> shapes;
+	//	std::vector<tinyobj::material_t> materials;
+	//	int materialId;
 
-		std::string err;
-		bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &err, fileName.c_str(), basePath.c_str(), GL_TRUE);
+	//	std::string err;
+	//	bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &err, fileName.c_str(), basePath.c_str(), GL_TRUE);
 
-		if (!err.empty()) {
+	//	if (!err.empty()) {
 
-			// `err` may contain warning message.
-			std::cerr << err << std::endl;
-		}
+	//		// `err` may contain warning message.
+	//		std::cerr << err << std::endl;
+	//	}
 
-		if (!ret) {
+	//	if (!ret) {
 
-			exit(1);
-		}
+	//		exit(1);
+	//	}
 
-		std::cout << "# of shapes    : " << shapes.size() << std::endl;
-		std::cout << "# of materials : " << materials.size() << std::endl;
+	//	std::cout << "# of shapes    : " << shapes.size() << std::endl;
+	//	std::cout << "# of materials : " << materials.size() << std::endl;
 
-		// Loop over shapes
-		for (size_t s = 0; s < shapes.size(); s++) {
+	//	// Loop over shapes
+	//	for (size_t s = 0; s < shapes.size(); s++) {
 
-			std::vector<gps::Vertex> vertices;
-			std::vector<GLuint> indices;
-			std::vector<gps::Texture> textures;
+	//		std::vector<gps::Vertex> vertices;
+	//		std::vector<GLuint> indices;
+	//		std::vector<gps::Texture> textures;
 
-			// Loop over faces(polygon)
-			size_t index_offset = 0;
-			for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
+	//		// Loop over faces(polygon)
+	//		size_t index_offset = 0;
+	//		for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
 
-				int fv = shapes[s].mesh.num_face_vertices[f];
+	//			int fv = shapes[s].mesh.num_face_vertices[f];
 
-				//gps::Texture currentTexture = LoadTexture("index1.png", "ambientTexture");
-				//textures.push_back(currentTexture);
+	//			//gps::Texture currentTexture = LoadTexture("index1.png", "ambientTexture");
+	//			//textures.push_back(currentTexture);
 
-				// Loop over vertices in the face.
-				for (size_t v = 0; v < fv; v++) {
+	//			// Loop over vertices in the face.
+	//			for (size_t v = 0; v < fv; v++) {
 
-					// access to vertex
-					tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+	//				// access to vertex
+	//				tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
 
-					float vx = attrib.vertices[3 * idx.vertex_index + 0];
-					float vy = attrib.vertices[3 * idx.vertex_index + 1];
-					float vz = attrib.vertices[3 * idx.vertex_index + 2];
-					float nx = attrib.normals[3 * idx.normal_index + 0];
-					float ny = attrib.normals[3 * idx.normal_index + 1];
-					float nz = attrib.normals[3 * idx.normal_index + 2];
-					float tx = 0.0f;
-					float ty = 0.0f;
+	//				float vx = attrib.vertices[3 * idx.vertex_index + 0];
+	//				float vy = attrib.vertices[3 * idx.vertex_index + 1];
+	//				float vz = attrib.vertices[3 * idx.vertex_index + 2];
+	//				float nx = attrib.normals[3 * idx.normal_index + 0];
+	//				float ny = attrib.normals[3 * idx.normal_index + 1];
+	//				float nz = attrib.normals[3 * idx.normal_index + 2];
+	//				float tx = 0.0f;
+	//				float ty = 0.0f;
 
-					if (idx.texcoord_index != -1) {
+	//				if (idx.texcoord_index != -1) {
 
-						tx = attrib.texcoords[2 * idx.texcoord_index + 0];
-						ty = attrib.texcoords[2 * idx.texcoord_index + 1];
-					}
+	//					tx = attrib.texcoords[2 * idx.texcoord_index + 0];
+	//					ty = attrib.texcoords[2 * idx.texcoord_index + 1];
+	//				}
 
-					glm::vec3 vertexPosition(vx, vy, vz);
-					glm::vec3 vertexNormal(nx, ny, nz);
-					glm::vec2 vertexTexCoords(tx, ty);
+	//				glm::vec3 vertexPosition(vx, vy, vz);
+	//				glm::vec3 vertexNormal(nx, ny, nz);
+	//				glm::vec2 vertexTexCoords(tx, ty);
 
-					gps::Vertex currentVertex;
-					currentVertex.Position = vertexPosition;
-					currentVertex.Normal = vertexNormal;
-					currentVertex.TexCoords = vertexTexCoords;
+	//				gps::Vertex currentVertex;
+	//				currentVertex.Position = vertexPosition;
+	//				currentVertex.Normal = vertexNormal;
+	//				currentVertex.TexCoords = vertexTexCoords;
 
-					vertices.push_back(currentVertex);
+	//				vertices.push_back(currentVertex);
 
-					indices.push_back((GLuint)(index_offset + v));
-				}
+	//				indices.push_back((GLuint)(index_offset + v));
+	//			}
 
-				index_offset += fv;
-			}
+	//			index_offset += fv;
+	//		}
 
-			// get material id
-			// Only try to read materials if the .mtl file is present
-			size_t a = shapes[s].mesh.material_ids.size();
+	//		// get material id
+	//		// Only try to read materials if the .mtl file is present
+	//		size_t a = shapes[s].mesh.material_ids.size();
 
-			if (a > 0 && materials.size()>0) {
+	//		if (a > 0 && materials.size()>0) {
 
-				materialId = shapes[s].mesh.material_ids[0];
-				if (materialId != -1) {
+	//			materialId = shapes[s].mesh.material_ids[0];
+	//			if (materialId != -1) {
 
-					gps::Material currentMaterial;
-					currentMaterial.ambient = glm::vec3(materials[materialId].ambient[0], materials[materialId].ambient[1], materials[materialId].ambient[2]);
-					currentMaterial.diffuse = glm::vec3(materials[materialId].diffuse[0], materials[materialId].diffuse[1], materials[materialId].diffuse[2]);
-					currentMaterial.specular = glm::vec3(materials[materialId].specular[0], materials[materialId].specular[1], materials[materialId].specular[2]);
+	//				gps::Material currentMaterial;
+	//				currentMaterial.ambient = glm::vec3(materials[materialId].ambient[0], materials[materialId].ambient[1], materials[materialId].ambient[2]);
+	//				currentMaterial.diffuse = glm::vec3(materials[materialId].diffuse[0], materials[materialId].diffuse[1], materials[materialId].diffuse[2]);
+	//				currentMaterial.specular = glm::vec3(materials[materialId].specular[0], materials[materialId].specular[1], materials[materialId].specular[2]);
 
-					//ambient texture
-					std::string ambientTexturePath = materials[materialId].ambient_texname;
+	//				//ambient texture
+	//				std::string ambientTexturePath = materials[materialId].ambient_texname;
 
-					if (!ambientTexturePath.empty()) {
+	//				if (!ambientTexturePath.empty()) {
 
-						gps::Texture currentTexture;
-						currentTexture = LoadTexture(basePath + ambientTexturePath, "ambientTexture");
-						textures.push_back(currentTexture);
-					}
+	//					gps::Texture currentTexture;
+	//					currentTexture = LoadTexture(basePath + ambientTexturePath, "ambientTexture");
+	//					textures.push_back(currentTexture);
+	//				}
 
-					//diffuse texture
-					std::string diffuseTexturePath = materials[materialId].diffuse_texname;
+	//				//diffuse texture
+	//				std::string diffuseTexturePath = materials[materialId].diffuse_texname;
 
-					if (!diffuseTexturePath.empty()) {
+	//				if (!diffuseTexturePath.empty()) {
 
-						gps::Texture currentTexture;
-						currentTexture = LoadTexture(basePath + diffuseTexturePath, "diffuseTexture");
-						textures.push_back(currentTexture);
-					}
+	//					gps::Texture currentTexture;
+	//					currentTexture = LoadTexture(basePath + diffuseTexturePath, "diffuseTexture");
+	//					textures.push_back(currentTexture);
+	//				}
 
-					//specular texture
-					std::string specularTexturePath = materials[materialId].specular_texname;
+	//				//specular texture
+	//				std::string specularTexturePath = materials[materialId].specular_texname;
 
-					if (!specularTexturePath.empty()) {
+	//				if (!specularTexturePath.empty()) {
 
-						gps::Texture currentTexture;
-						currentTexture = LoadTexture(basePath + specularTexturePath, "specularTexture");
-						textures.push_back(currentTexture);
-					}
-				}
-			}
+	//					gps::Texture currentTexture;
+	//					currentTexture = LoadTexture(basePath + specularTexturePath, "specularTexture");
+	//					textures.push_back(currentTexture);
+	//				}
+	//			}
+	//		}
 
-			meshes.push_back(gps::Mesh(vertices, indices, textures));
-		}
+	//		meshes.push_back(gps::Mesh(vertices, indices, textures));
+	//	}
+	//}
+void Model3D::ReadOBJ(std::string fileName, std::string basePath) {
+
+	std::cout << "Loading : " << fileName << std::endl;
+	tinyobj::attrib_t attrib;
+	std::vector<tinyobj::shape_t> shapes;
+	std::vector<tinyobj::material_t> materials;
+	int materialId;
+
+	std::string err;
+	bool ret = tinyobj::LoadObj(&attrib, &shapes, &materials, &err, fileName.c_str(), basePath.c_str(), GL_TRUE);
+
+	if (!err.empty()) {
+		std::cerr << err << std::endl;
 	}
+
+	if (!ret) {
+		exit(1);
+	}
+
+	std::cout << "# of shapes    : " << shapes.size() << std::endl;
+	std::cout << "# of materials : " << materials.size() << std::endl;
+
+	// Loop over shapes
+	for (size_t s = 0; s < shapes.size(); s++) {
+
+		std::vector<gps::Vertex> vertices;
+		std::vector<GLuint> indices;
+		std::vector<gps::Texture> textures;
+
+		// --- DEBUG START: ---
+		if (shapes[s].mesh.material_ids.size() > 0) {
+			int currentMatID = shapes[s].mesh.material_ids[0];
+			if (currentMatID != -1) {
+				
+				std::string matName = materials[currentMatID].name;
+				std::string texName = materials[currentMatID].diffuse_texname;
+				if (matName.find("Twigs") != std::string::npos || matName.find("Roof") != std::string::npos || matName.find("005") != std::string::npos) {
+					std::cout << "!!! DEBUG !!! Shape: " << shapes[s].name << " uses Material: " << matName << std::endl;
+					std::cout << "   -> Texture Path in MTL: '" << texName << "'" << std::endl;
+				}
+			}
+		}
+		// --- DEBUG END ---
+
+		// Loop over faces(polygon)
+		size_t index_offset = 0;
+		for (size_t f = 0; f < shapes[s].mesh.num_face_vertices.size(); f++) {
+
+			int fv = shapes[s].mesh.num_face_vertices[f];
+
+			// Loop over vertices in the face.
+			for (size_t v = 0; v < fv; v++) {
+
+				// access to vertex
+				tinyobj::index_t idx = shapes[s].mesh.indices[index_offset + v];
+
+				float vx = attrib.vertices[3 * idx.vertex_index + 0];
+				float vy = attrib.vertices[3 * idx.vertex_index + 1];
+				float vz = attrib.vertices[3 * idx.vertex_index + 2];
+				float nx = attrib.normals[3 * idx.normal_index + 0];
+				float ny = attrib.normals[3 * idx.normal_index + 1];
+				float nz = attrib.normals[3 * idx.normal_index + 2];
+				float tx = 0.0f;
+				float ty = 0.0f;
+
+				if (idx.texcoord_index != -1) {
+					tx = attrib.texcoords[2 * idx.texcoord_index + 0];
+					ty = attrib.texcoords[2 * idx.texcoord_index + 1];
+				}
+
+				glm::vec3 vertexPosition(vx, vy, vz);
+				glm::vec3 vertexNormal(nx, ny, nz);
+				glm::vec2 vertexTexCoords(tx, ty);
+
+				gps::Vertex currentVertex;
+				currentVertex.Position = vertexPosition;
+				currentVertex.Normal = vertexNormal;
+				currentVertex.TexCoords = vertexTexCoords;
+
+				vertices.push_back(currentVertex);
+				indices.push_back((GLuint)(index_offset + v));
+			}
+
+			index_offset += fv;
+		}
+
+		// get material id
+		size_t a = shapes[s].mesh.material_ids.size();
+
+		if (a > 0 && materials.size() > 0) {
+
+			materialId = shapes[s].mesh.material_ids[0];
+			if (materialId != -1) {
+
+				gps::Material currentMaterial;
+				currentMaterial.ambient = glm::vec3(materials[materialId].ambient[0], materials[materialId].ambient[1], materials[materialId].ambient[2]);
+				currentMaterial.diffuse = glm::vec3(materials[materialId].diffuse[0], materials[materialId].diffuse[1], materials[materialId].diffuse[2]);
+				currentMaterial.specular = glm::vec3(materials[materialId].specular[0], materials[materialId].specular[1], materials[materialId].specular[2]);
+
+				//ambient texture
+				std::string ambientTexturePath = materials[materialId].ambient_texname;
+				if (!ambientTexturePath.empty()) {
+					gps::Texture currentTexture;
+					currentTexture = LoadTexture(basePath + ambientTexturePath, "ambientTexture");
+					textures.push_back(currentTexture);
+				}
+
+				//diffuse texture
+				std::string diffuseTexturePath = materials[materialId].diffuse_texname;
+				if (!diffuseTexturePath.empty()) {
+					gps::Texture currentTexture;
+					currentTexture = LoadTexture(basePath + diffuseTexturePath, "diffuseTexture");
+					textures.push_back(currentTexture);
+				}
+
+				//specular texture
+				std::string specularTexturePath = materials[materialId].specular_texname;
+				if (!specularTexturePath.empty()) {
+					gps::Texture currentTexture;
+					currentTexture = LoadTexture(basePath + specularTexturePath, "specularTexture");
+					textures.push_back(currentTexture);
+				}
+			}
+		}
+
+		meshes.push_back(gps::Mesh(vertices, indices, textures));
+	}
+}
 
 	// Retrieves a texture associated with the object - by its name and type
 	gps::Texture Model3D::LoadTexture(std::string path, std::string type) {
--- GraphicScene_OpenGL/main.cpp
+++ GraphicScene_OpenGL/main.cpp
@@ -52,11 +52,26 @@ GLfloat cameraSpeed = 0.1f;
 GLboolean pressedKeys[1024];
 
 // models
-gps::Model3D teapot;
-GLfloat angle;
+gps::Model3D myScene;
+
+// Mouse control variables
+float lastX = 512.0f; 
+float lastY = 384.0f; 
+float yaw = -90.0f;   
+float pitch = 0.0f;  
+bool firstMouse = true; 
+float sensitivity = 0.1f; 
 
 // shaders
 gps::Shader myBasicShader;
+
+// Wireframe mode
+bool wireframeMode = false;
+
+bool isFullscreen = false;
+// Store the window state to restore it later
+int savedXPos, savedYPos;
+int savedWidth, savedHeight;
 
 GLenum glCheckError_(const char* file, int line)
 {
@@ -88,12 +103,28 @@ GLenum glCheckError_(const char* file, int line)
 
 void windowResizeCallback(GLFWwindow* window, int width, int height) {
     fprintf(stdout, "Window resized! New width: %d , and height: %d\n", width, height);
-    //TODO
+
+    if (height == 0) height = 1;
+
+    glViewport(0, 0, width, height);
+
+    projection = glm::perspective(glm::radians(45.0f), (float)width / (float)height, 0.1f, 1000.0f);
+
+    myBasicShader.useShaderProgram();
+    glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 }
 
 void keyboardCallback(GLFWwindow* window, int key, int scancode, int action, int mode) {
     if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
         glfwSetWindowShouldClose(window, GL_TRUE);
+    }
+
+    if (key == GLFW_KEY_M && action == GLFW_PRESS) {
+        wireframeMode = !wireframeMode;
+        if (wireframeMode)
+            glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+        else
+            glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
     }
 
     if (key >= 0 && key < 1024) {
@@ -104,161 +135,146 @@ void keyboardCallback(GLFWwindow* window, int key, int scancode, int action, int
             pressedKeys[key] = false;
         }
     }
+
+    if (key == GLFW_KEY_F10 && action == GLFW_PRESS) {
+        if (!isFullscreen) {
+            glfwGetWindowPos(window, &savedXPos, &savedYPos);
+            glfwGetWindowSize(window, &savedWidth, &savedHeight);
+
+            GLFWmonitor* monitor = glfwGetPrimaryMonitor();
+            const GLFWvidmode* mode = glfwGetVideoMode(monitor);
+            glfwSetWindowMonitor(window, monitor, 0, 0, mode->width, mode->height, mode->refreshRate);
+        }
+        else {
+            glfwSetWindowMonitor(window, nullptr, savedXPos, savedYPos, savedWidth, savedHeight, 0);
+        }
+        isFullscreen = !isFullscreen;
+    }
 }
 
 void mouseCallback(GLFWwindow* window, double xpos, double ypos) {
-    //TODO
+    if (firstMouse) {
+        lastX = (float)xpos;
+        lastY = (float)ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = (float)xpos - lastX;
+    float yoffset = lastY - (float)ypos; 
+    lastX = (float)xpos;
+    lastY = (float)ypos;
+
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    if (pitch > 89.0f) pitch = 89.0f;
+    if (pitch < -89.0f) pitch = -89.0f;
+
+    myCamera.rotate(pitch, yaw);
 }
 
 void processMovement() {
     if (pressedKeys[GLFW_KEY_W]) {
         myCamera.move(gps::MOVE_FORWARD, cameraSpeed);
-        //update view matrix
-        view = myCamera.getViewMatrix();
-        myBasicShader.useShaderProgram();
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-        // compute normal matrix for teapot
-        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
     }
-
     if (pressedKeys[GLFW_KEY_S]) {
         myCamera.move(gps::MOVE_BACKWARD, cameraSpeed);
-        //update view matrix
-        view = myCamera.getViewMatrix();
-        myBasicShader.useShaderProgram();
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-        // compute normal matrix for teapot
-        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
     }
-
     if (pressedKeys[GLFW_KEY_A]) {
         myCamera.move(gps::MOVE_LEFT, cameraSpeed);
-        //update view matrix
-        view = myCamera.getViewMatrix();
-        myBasicShader.useShaderProgram();
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-        // compute normal matrix for teapot
-        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
     }
-
     if (pressedKeys[GLFW_KEY_D]) {
         myCamera.move(gps::MOVE_RIGHT, cameraSpeed);
-        //update view matrix
-        view = myCamera.getViewMatrix();
-        myBasicShader.useShaderProgram();
-        glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
-        // compute normal matrix for teapot
-        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
-    }
-
-    if (pressedKeys[GLFW_KEY_Q]) {
-        angle -= 1.0f;
-        // update model matrix for teapot
-        model = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0, 1, 0));
-        // update normal matrix for teapot
-        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
-    }
-
-    if (pressedKeys[GLFW_KEY_E]) {
-        angle += 1.0f;
-        // update model matrix for teapot
-        model = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0, 1, 0));
-        // update normal matrix for teapot
-        normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
     }
 }
 
 void initOpenGLWindow() {
-    myWindow.Create(1024, 768, "OpenGL Project Core");
+    myWindow.Create(1024, 768, "OpenGL Project - Village");
 }
-
 void setWindowCallbacks() {
     glfwSetWindowSizeCallback(myWindow.getWindow(), windowResizeCallback);
     glfwSetKeyCallback(myWindow.getWindow(), keyboardCallback);
     glfwSetCursorPosCallback(myWindow.getWindow(), mouseCallback);
+
+    glfwSetInputMode(myWindow.getWindow(), GLFW_CURSOR, GLFW_CURSOR_DISABLED);
 }
 
 void initOpenGLState() {
-    glClearColor(0.7f, 0.7f, 0.7f, 1.0f);
+    glClearColor(0.5f, 0.7f, 1.0f, 1.0f); //bright blue
     glViewport(0, 0, myWindow.getWindowDimensions().width, myWindow.getWindowDimensions().height);
     glEnable(GL_FRAMEBUFFER_SRGB);
-    glEnable(GL_DEPTH_TEST); // enable depth-testing
-    glDepthFunc(GL_LESS); // depth-testing interprets a smaller value as "closer"
-    glEnable(GL_CULL_FACE); // cull face
-    glCullFace(GL_BACK); // cull back face
-    glFrontFace(GL_CCW); // GL_CCW for counter clock-wise
+    glEnable(GL_DEPTH_TEST); 
+    glDepthFunc(GL_LESS);
+    glCullFace(GL_BACK);
+    glFrontFace(GL_CCW);
+
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
 }
 
 void initModels() {
-    teapot.LoadModel("models/teapot/teapot20segUT.obj");
+    myScene.LoadModel("models/village/village.obj");
 }
 
 void initShaders() {
-    myBasicShader.loadShader(
-        "shaders/basic.vert",
-        "shaders/basic.frag");
+    myBasicShader.loadShader("shaders/basic.vert", "shaders/basic.frag");
 }
 
 void initUniforms() {
     myBasicShader.useShaderProgram();
 
-    // create model matrix for teapot
-    model = glm::rotate(glm::mat4(1.0f), glm::radians(angle), glm::vec3(0.0f, 1.0f, 0.0f));
+    // Model matrix 
+    model = glm::mat4(1.0f);
     modelLoc = glGetUniformLocation(myBasicShader.shaderProgram, "model");
+    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
 
-    // get view matrix for current camera
+    // View matrix camera
     view = myCamera.getViewMatrix();
     viewLoc = glGetUniformLocation(myBasicShader.shaderProgram, "view");
-    // send view matrix to shader
     glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 
-    // compute normal matrix for teapot
+    // Normal matrix for light
     normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
     normalMatrixLoc = glGetUniformLocation(myBasicShader.shaderProgram, "normalMatrix");
+    glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
 
-    // create projection matrix
+    // Projection matrix camera lens
     projection = glm::perspective(glm::radians(45.0f),
         (float)myWindow.getWindowDimensions().width / (float)myWindow.getWindowDimensions().height,
-        0.1f, 20.0f);
+        0.1f, 1000.0f); 
     projectionLoc = glGetUniformLocation(myBasicShader.shaderProgram, "projection");
-    // send projection matrix to shader
     glUniformMatrix4fv(projectionLoc, 1, GL_FALSE, glm::value_ptr(projection));
 
-    //set the light direction (direction towards the light)
-    lightDir = glm::vec3(0.0f, 1.0f, 1.0f);
+    // sun
+    lightDir = glm::vec3(0.5f, 1.0f, 0.5f); // light can bee seen from above and sides
     lightDirLoc = glGetUniformLocation(myBasicShader.shaderProgram, "lightDir");
-    // send light dir to shader
     glUniform3fv(lightDirLoc, 1, glm::value_ptr(lightDir));
 
-    //set light color
-    lightColor = glm::vec3(1.0f, 1.0f, 1.0f); //white light
+    lightColor = glm::vec3(1.0f, 1.0f, 1.0f); // white light
     lightColorLoc = glGetUniformLocation(myBasicShader.shaderProgram, "lightColor");
-    // send light color to shader
     glUniform3fv(lightColorLoc, 1, glm::value_ptr(lightColor));
 }
 
-void renderTeapot(gps::Shader shader) {
-    // select active shader program
-    shader.useShaderProgram();
-
-    //send teapot model matrix data to shader
-    glUniformMatrix4fv(modelLoc, 1, GL_FALSE, glm::value_ptr(model));
-
-    //send teapot normal matrix data to shader
-    glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
-
-    // draw teapot
-    teapot.Draw(shader);
-}
-
-void renderScene() {
+void renderScene(gps::Shader shader) {
+    // Clear las frame
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-    //render the scene
+    // activate shader
+    shader.useShaderProgram();
 
-    // render the teapot
-    renderTeapot(myBasicShader);
+    // view matrix update for movement
+    view = myCamera.getViewMatrix();
+    glUniformMatrix4fv(viewLoc, 1, GL_FALSE, glm::value_ptr(view));
 
+    // normal matrix update for light calc when we rotate
+    normalMatrix = glm::mat3(glm::inverseTranspose(view * model));
+    glUniformMatrix3fv(normalMatrixLoc, 1, GL_FALSE, glm::value_ptr(normalMatrix));
+
+    // draw the village
+    myScene.Draw(shader);
 }
 
 void cleanup() {
@@ -286,7 +302,7 @@ int main(int argc, const char* argv[]) {
     // application loop
     while (!glfwWindowShouldClose(myWindow.getWindow())) {
         processMovement();
-        renderScene();
+        renderScene(myBasicShader);
 
         glfwPollEvents();
         glfwSwapBuffers(myWindow.getWindow());
